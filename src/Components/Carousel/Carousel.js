989d8bcfed9788bae20760eb834b018adeae33c1
Milad Ghadami Yazdi <milad.ghadami@appliedtechnology.se>
2020-06-10 15:17:00 +0200

    Implement some logic

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React , {useState} from 'react'
 import "./Carousel.css"
 
 const Carousel = () => {
    let imageArr = [1, 2, 3, 4, 5, 6]  // using numbers, implementing images later
-   const goToPrevImage = () => {
-
+   const lengthToCover = (1-imageArr.length)*100
+   const [xcoord, setXcoord] = useState(0)
+   let imageStyle = {
+       transform: `translateX(${xcoord}%)`,
+       backgroundColor: "#BFFFFF"
    }
-   const goToNextImage = () => {
+   const goToPrev = () => {
+    xcoord === 0 ? setXcoord(lengthToCover) : setXcoord(xcoord + 100) 
+   }
+   const goToNext = () => {
+    xcoord !== lengthToCover ? setXcoord(xcoord - 100) : setXcoord(0)
        
 }
     return (
         <div className = "carousel">
-            <button className = "prev-image-btn btn">Prev</button>
+            <button className = "prev-image-btn btn" onClick={goToPrev}>Prev</button>
             {imageArr.map((image, i) => {
-                return <div key={i} className = "image-slide">{image}</div>
+                return <div key={i} className = "image-slide" style={imageStyle}>{image}</div>
             })}
-            <button className = "next-image-btn btn">Next</button>
+            <button className = "next-image-btn btn" onClick={goToNext}>Next</button>
 
         </div>
     )
